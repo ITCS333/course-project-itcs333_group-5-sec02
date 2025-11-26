f67b9794f98b6cca03e37536edb67da82c44768f
shaima <shaimaabdulrahim@yahoo.com>
2025-11-26 13:26:49 +0300

    Handle preflight

--- src/discussion/api/index.php
+++ src/discussion/api/index.php
@@ -101,10 +101,18 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization");
 // Allow cross-origin requests (CORS) if needed
 // Allow specific HTTP methods (GET, POST, PUT, DELETE, OPTIONS)
 // Allow specific headers (Content-Type, Authorization)
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");  
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 
 // TODO: Handle preflight OPTIONS request
 // If the request method is OPTIONS, return 200 status and exit
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 
 // TODO: Include the database connection class
