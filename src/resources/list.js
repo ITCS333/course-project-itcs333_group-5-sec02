fb5ace32f19fc51b44d1b69c72c437cab695c3a9
Zainab Almosawi <202309301@stu.uob.edu.bh>
2025-12-06 15:23:40 +0300

    update 2

--- src/resources/list.js
+++ src/resources/list.js
@@ -51,7 +51,7 @@ function createResourceArticle(resource) {
 async function loadResources() {
   // ... your implementation here ...
   try {
-    const response = await fetch("resources.json");
+    const response = await fetch("api/resources.json");
     const resources = await response.json();
 
     listSection.innerHTML = "";
